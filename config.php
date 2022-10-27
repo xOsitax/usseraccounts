061a0864661f119b1d4ef3f3d7a216fdf1fdadca
YesseniaC <98070263+xOsitax@users.noreply.github.com>
2022-10-27 17:26:02 -0400

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 $db_host = "us-cdbr-east-05.cleardb.net";
 $db_user =  "bb51f1b46e281b";
 $db_pass =  "acccf375";
-$db_name =  " heroku_bc7b5a7cff19790"
+$db_name =  " heroku_bc7b5a7cff19790";
 
 $connect = mysqli_connect ($db_host, $db_user, $db_pass, $db_name) or die ("database connection error");
